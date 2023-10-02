3999aa926d727cd330f3ae0f90c3e50c01dc0936
dolgi <dolgiholeg@rambler.ru>
2023-10-03 02:03:02 +0700

    03102023

--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
-from .models import *
+from .models import*
+from django.contrib.auth.models import User
 
 def index(req):
     numkino = Kino.objects.all().count()
     numactor = Actor.objects.all().count()
     numfree = Kino.objects.filter(status__kino=1).count()
-    data = {'k1': numkino, 'k2': numactor, 'k3': numfree}
+    if req.user.username:
+        username = req.user.first_name
+    else:
+        username = 'ГОСТЬ'
+    data = {'k1': numkino, 'k2': numactor, 'k3': numfree, 'username': username}
+    #user = User.objects.create_user('User2','user2@mail.ru', 'useruser')
+    #user.first_name = 'Vlad'
+    #user.last_name = 'Petrov'
+    #user.save()
+
     return render(req, 'index.html', context=data)
 
 from django.views import generic
 class Kinolist(generic.ListView):
     model = Kino
+    paginate_by = 2
 
 class KinoDetail(generic.DetailView):
     model = Kino
