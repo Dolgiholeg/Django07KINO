2b7012959c86e9a3eb9e70343675d52e2851df5f
dolgi <dolgiholeg@rambler.ru>
2023-10-12 01:52:26 +0700

    11102023-2

--- catalog/views.py
+++ catalog/views.py
@@ -41,30 +41,30 @@ class DirectorDetail(generic.DetailView):
     model = Director
 
 from django.http import HttpResponse
-def info(req,id):
+def info(req, id):
     film = Kino.objects.get(id=id)
     return HttpResponse(film.title)
 
 def status(req):
     k1 = Status.objects.all()
-    data = {'podpiska':k1}
+    data = {'podpiska': k1}
     return render(req, 'podpiska.html', data)
 
 def prosmotr(req, id1, id2, id3):
     status = 0
     print(id1, id2, id3)
-    mas = ['бесплатная','базовая','супер']#kino id2
-    mas2 = ['Free','Base','Super']#user id3 status
+    mas = ['бесплатная', 'базовая', 'супер'] #kino id2
+    mas2 = ['Free', 'Base', 'Super'] #user id3 status
     if id3 != 0:
-        status = User.objects.get(id=id3)#нашли юзера
-        status = status.groups.all()#нашли его подписки
-        status = status[0].id#нашли айди его подписки(она одна)
+        status = User.objects.get(id=id3) #нашли юзера
+        status = status.groups.all() #нашли его подписки
+        status = status[0].id #нашли айди его подписки(она одна)
         print(status)
     else:
-        if id3 == 0:#выдаёт гостю подписку номер1 - бесплатную
+        if id3 == 0: #выдаёт гостю подписку номер1 - бесплатную
             status = 1
 
-    if status >= id2:#сравниваем статус и разрешение на просмотр фильма
+    if status >= id2: #сравниваем статус и разрешение на просмотр фильма
         print('OK')
         permission = True
     else:
@@ -74,17 +74,51 @@ def prosmotr(req, id1, id2, id3):
     k1 = Kino.objects.get(id=id1).title
     k2 = Group.objects.get(id=status).name
     k3 = Status.objects.get(id=id2).name
-    data={'kino':k1,'status':k2,'statuskino':k3, 'prava': permission}
-    return render(req,'prosmotr.html', data)
+    data = {'kino': k1, 'status': k2, 'statuskino': k3, 'prava': permission}
+    return render(req, 'prosmotr.html', data)
 
 def buy(req, type):
-    usid = req.user.id#находим номер текущего пользователя
-    user123 = User.objects.get(id=usid)#находим его в табличке user
-    statusnow = user123.groups.all()[0].id#номер его подписки(группы)
-    grold = Group.objects.get(id=statusnow)#находим его подписку в таблице group
-    grold.user_set.remove(user123)#удаляем старую подписку
-    grnew = Group.objects.get(id=type)#находим новую подписку
-    grnew.user_set.add(user123)#добавляем новую подписку
+    usid = req.user.id #находим номер текущего пользователя
+    user123 = User.objects.get(id=usid) #находим его в табличке user
+    statusnow = user123.groups.all()[0].id #номер его подписки(группы)
+    grold = Group.objects.get(id=statusnow) #находим его подписку в таблице group
+    grold.user_set.remove(user123) #удаляем старую подписку
+    grnew = Group.objects.get(id=type) #находим новую подписку
+    grnew.user_set.add(user123) #добавляем новую подписку
     k1 = grnew.name
     data = {'podpiska': k1}
-    return  render (req,'buy.html',data)
+    return render(req, 'buy.html', data)
+
+from .form import SignUpform
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+def registr(req):
+    print(1)
+    if req.POST:
+        print(2)
+        anketa = SignUpform(req.POST)
+        if anketa.is_valid():
+            print(3)
+            anketa.save()
+            k1 = anketa.cleaned_data.get('username')
+            k2 = anketa.cleaned_data.get('password1')
+            k3 = anketa.cleaned_data.get('first_name')
+            k4 = anketa.cleaned_data.get('last_name')
+            k5 = anketa.cleaned_data.get('email')
+            user = authenticate(username=k1, password=k2) #сохраняет нового пользоватлея
+            man = User.objects.get(username=k1)             #найдем нового юзера
+            #заполним поля в таблице
+            man.email=k5
+            man.first_name=k3
+            man.last_name=k4
+            man.save()
+            login(req,user) #входит на сайт
+            group = Group.objects.get(id=1)#находим бесплатную подписку
+            group.user_set.add(man)#записываем юзера в подписку
+            return redirect('home')
+
+    else:
+        anketa = SignUpform()
+    data={'regform':anketa}
+    return render(req,'registration/registration.html',context=data)
